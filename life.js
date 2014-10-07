02feb9b8b496c6ceb2747bc16e8e380b8e0f6f45
Gerald Spreer <geraldspreer@gmail.com>
2014-10-07 09:37:39 +0200

    infinite board

--- life.js
+++ life.js
@@ -1,11 +1,11 @@
 $( document ).ready( function() {
 
-	var BOARD_SIZE = 20;
+	var BOARD_SIZE = 40;
 
 	 var drawTimer =	setInterval( function() { cycle(); }, 50);
-		
+
  	//
-	// controls 
+	// controls
 	//
 	$("#stop").click( function(){ clearInterval( drawTimer ); });
 	$("#cycle").click( function(){ cycle(); });
@@ -20,61 +20,83 @@ $( document ).ready( function() {
 
 			creation[i] = Math.round(Math.random());
 		};
-	
+
 
 	function cycle() {
-	
-		var liveCount = 0;
+
+		var lifeCount = 0;
 
 		for (var i = 0; i < creation.length; i++) {
 
-			liveCount = 0;	
-			
+			lifeCount = 0;
+			//
 			// clipping
+			//
 			if ( i < BOARD_SIZE) {
-				nextStep[i] = 0;	
-			} else if ( i > 380 ) {
-				nextStep[i] = 0;	
-			} else {
 
-				liveCount = creation[i-1] ; 
-				liveCount += creation[i-BOARD_SIZE] ;
-				liveCount += creation[  i- ( BOARD_SIZE  - 1 )] ;
-				liveCount += creation[  i- ( BOARD_SIZE  + 1 )] ; 
-				liveCount += creation[i + 1] ; 
-				liveCount += creation[  i + BOARD_SIZE ] ; 
-				liveCount += creation[  i + ( BOARD_SIZE  - 1 )] ; 
-				liveCount += creation[  i + ( BOARD_SIZE  + 1) ];
+				var p = BOARD_SIZE * BOARD_SIZE - i;
+
+				lifeCount = creation[p-1] ;
+				lifeCount += creation[p-BOARD_SIZE] ;
+				lifeCount += creation[  p- ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  p- ( BOARD_SIZE  + 1 )] ;
+				lifeCount += creation[i + 1] ;
+				lifeCount += creation[  i + BOARD_SIZE ] ;
+				lifeCount += creation[  i + ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  i + ( BOARD_SIZE  + 1) ];
+			
+			} else if ( i > BOARD_SIZE * BOARD_SIZE - BOARD_SIZE ) {
+
+				var p = i - BOARD_SIZE * BOARD_SIZE + BOARD_SIZE;
+
+				lifeCount = creation[i-1] ; 
+				lifeCount += creation[i-BOARD_SIZE] ;
+				lifeCount += creation[  i- ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  i- ( BOARD_SIZE  + 1 )] ;
+				lifeCount += creation[i + 1] ;
+				lifeCount += creation[  p + BOARD_SIZE ] ;
+				lifeCount += creation[  p + ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  p + ( BOARD_SIZE  + 1) ];
+      } else {
+				lifeCount = creation[i-1] ;
+				lifeCount += creation[i-BOARD_SIZE] ;
+				lifeCount += creation[  i- ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  i- ( BOARD_SIZE  + 1 )] ;
+				lifeCount += creation[i + 1] ;
+				lifeCount += creation[  i + BOARD_SIZE ] ;
+				lifeCount += creation[  i + ( BOARD_SIZE  - 1 )] ;
+				lifeCount += creation[  i + ( BOARD_SIZE  + 1) ];
+
 			}
 
 			if ( creation[i] == 1 ) {
-				if ( liveCount < 2 || liveCount > 3) {
+				if ( lifeCount < 2 || lifeCount > 3) {
 						nextStep[i] = 0;
 					}
-				else if ( liveCount == 2 || liveCount == 3 ) {
+				else if ( lifeCount == 2 || lifeCount == 3 ) {
 						nextStep[i] = 1;
 					}
-			} else { 
-				if ( liveCount == 3 ) {
+			} else {
+				if ( lifeCount == 3 ) {
 					nextStep[i] = 1;
-				}  else { 
-					nextStep[i] = 0; 
+				}  else {
+					nextStep[i] = 0;
 					}
 				}
-		} 
+		}
 
 		drawBoard(nextStep);
-		
-		// copy current state of array 
+
+		// copy current state of array
 		creation = nextStep.slice();
 
 	}
 
 	function drawBoard(cells) {
-	
+
 		// clear board
 		$("#container").children().remove();
-	
+
 		// draw all cells from array
 		var x = 0;
 	   	for	(var index = 0; index < cells.length; index++) {
@@ -82,14 +104,14 @@ $( document ).ready( function() {
 				var p =	cells[index].toString();
 			} else { var p = "0" }
 
-			$("#container").append("<div class='cell _" + p + "'></div>");	
-			
+			$("#container").append("<div class='cell _" + p + "'></div>");
+
 			x += 1;
 			if ( x == BOARD_SIZE ) {
-				$("#container").append("<br />");	
+				$("#container").append("<br />");
 				x = 0;
 			}
-		} 	
+		}
 	}
 
 });
